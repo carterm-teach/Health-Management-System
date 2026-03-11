8478e87034f329c372e39e9253d6ecda4ac351d4
Teach2026 <44346823+carterm-teach@users.noreply.github.com>
2026-03-11 09:01:15 -0500

    Added Patient Header File
    This was done by Tochi

---
+++ Patient.h
@@ -0,0 +1,38 @@
+#include "User.h"
+#include "Insurance.h"
+#include "MedicalRecord.h"
+#include "Patient.h"
+#include "Doctor.h"
+#include "HealthcareSystem.h"
+#include <iostream>
+using namespace std;
+Patient::Patient() : User(), insurance(nullptr) {}
+Patient::Patient(int id, string name, string email, Insurance* ins)
+: User(id, name, email) {
+insurance = ins;
+}
+Patient::Patient(const Patient& other) 
+: User(other) {
+insurance = other.insurance; 
+for (auto record : other.medicalHistory) {
+medicalHistory.push_back(new MedicalRecord(*record));
+}
+}
+Patient::~Patient() {
+for (auto record : medicalHistory) {
+delete record;
+}
+medicalHistory.clear();
+}
+void Patient::requestAppointment(HealthcareSystem& system, Doctor* doctor)
+{
+system.scheduleAppointment(this, doctor);
+}
+void Patient::viewMedicalHistory() {
+for (auto record : medicalHistory) {
+record->displayRecord();
+}
+}
+void Patient::addMedicalRecord(const MedicalRecord& record) {
+medicalHistory.push_back(new MedicalRecord(record));
+}
