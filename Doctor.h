38841f22042baed01d3ac3fb9e7e08c86eabbbe4
Teach2026 <44346823+carterm-teach@users.noreply.github.com>
2026-03-11 13:48:41 -0500

    Create Doctor.h
    made by damien

---
+++ Doctor.h
@@ -0,0 +1,83 @@
+#ifndef DOCTOR_H
+
+#define DOCTOR_H
+
+
+
+#include <string>
+
+#include <vector>
+
+#include "User.h"
+
+
+
+class Appointment;
+
+class MedicalRecord;
+
+class Prescription;
+
+class Patient;
+
+
+
+class Doctor : public User {
+
+private:
+
+  std::string name;
+
+  std::string specialty;
+
+  std::vector<Appointment*> schedule;  
+
+
+
+public:
+
+ 
+
+  Doctor();
+
+  Doctor(const std::string& userID,
+
+      const std::string& name,
+
+      const std::string& email,
+
+      const std::string& specialty);
+
+  Doctor(const Doctor& other);
+
+  ~Doctor();
+
+
+
+  bool approveAppointment(Appointment& appointment);
+
+  void denyAppointment(Appointment& appointment);
+
+
+
+
+
+  MedicalRecord createMedicalRecord(Patient& patient,
+
+                   const std::string& diagnosis,
+
+                   const std::string& notes);
+
+
+
+  Prescription issuePrescription(Patient& patient,
+
+                  const std::string& medication,
+
+                  const std::string& dosage);
+
+
+
+  void displayInfo() const;
+
+};
