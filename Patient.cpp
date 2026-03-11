125fbb1af2d2ada6a5d5bca3a83d7ea31dd6ebf7
Teach2026 <44346823+carterm-teach@users.noreply.github.com>
2026-03-11 09:06:40 -0500

    Updated code from .h fill to .cpp file.

--- Patient.cpp
+++ Patient.cpp
@@ -1,24 +1,60 @@
-#ifndef PATIENT_H
-#define PATIENT_H
+
+Fit Page
+
+
+
+Drawing
+
+
+
+Image
+
+
+
+
+
+
+
+Line
+
+
+
+
+
 #include "User.h"
 #include "Insurance.h"
 #include "MedicalRecord.h"
 #include "Patient.h"
 #include "Doctor.h"
 #include "HealthcareSystem.h"
-#include <vector>
-#include <string>
-class Patient : public User {
-private:
-Insurance* insurance; 
-std::vector<MedicalRecord*> medicalHistory; 
-public:
-Patient();
-Patient(int id, std::string name, std::string email, Insurance* insurance);
-Patient(const Patient& other);
-~Patient();
-void requestAppointment(HealthcareSystem& system, Doctor* doctor);
-void viewMedicalHistory();
-void addMedicalRecord(const MedicalRecord& record);
-};
-#end
+#include <iostream>
+using namespace std;
+Patient::Patient() : User(), insurance(nullptr) {}
+Patient::Patient(int id, string name, string email, Insurance* ins)
+: User(id, name, email) {
+insurance = ins;
+}
+Patient::Patient(const Patient& other) 
+: User(other) {
+insurance = other.insurance; 
+for (auto record : other.medicalHistory) {
+medicalHistory.push_back(new MedicalRecord(*record));
+}
+}
+Patient::~Patient() {
+for (auto record : medicalHistory) {
+delete record;
+}
+medicalHistory.clear();
+}
+void Patient::requestAppointment(HealthcareSystem& system, Doctor* doctor)
+{
+system.scheduleAppointment(this, doctor);
+}
+void Patient::viewMedicalHistory() {
+for (auto record : medicalHistory) {
+record->displayRecord();
+}
+}
+void Patient::addMedicalRecord(const MedicalRecord& record) {
+medicalHistory.push_back(new MedicalRecord(record));
