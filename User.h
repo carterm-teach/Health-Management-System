36465fa93b837fb165232b0a98670bf5b84ff167
Teach2026 <44346823+carterm-teach@users.noreply.github.com>
2026-03-14 09:12:49 -0500

    Create User.h
    made by Mr. Carter

---
+++ User.h
@@ -0,0 +1,38 @@
+#ifndef User_hpp
+#define User_hpp
+
+#include <stdio.h>
+#include <string>
+#include <vector>
+using namespace std;
+class User{
+
+public:
+    int userID;
+    string name;
+    string email;
+    static int totalUsers;
+    static vector <User> users;
+    
+    User();
+    User(int,string,string);
+    ~User();
+    
+    void setuserId(int);
+    void setname(string);
+    void setemail(string);
+    int getuserID()const;
+    string getname()const;
+    string getemail()const;
+    
+    static int getTotalUsers();
+    
+    void addUser();
+    //void deleteUser();
+    
+    void Login()const;
+    void Logout()const;
+    void DisplayUserInfo()const;
+    
+};
+#endif /* User_hpp */
