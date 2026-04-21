3c12ec45dabb88f88f6a90971d1de9fbc049a2dd
Bobo-creator <demmyogunbade@gmail.com>
2026-04-20 22:07:59 -0500

    Fixed incomplete-type errors

--- MedicalRecord.h
+++ MedicalRecord.h
@@ -5,21 +5,22 @@
 
 #ifndef MedicalRecord_h
 #define MedicalRecord_h
-#include "Patient.h"
-#include "Doctor.h"
 #include <string>
+using namespace std;
+class Patient;
+class Doctor;
 class MedicalRecord{
 private:
     int recordID;
-    Patient patient;
-    Doctor doctor;
+    Patient* patient;
+    Doctor* doctor;
     string diagnosis;
     string notes;
     string date;
-    
+
 public:
     MedicalRecord();
-    MedicalRecord(int, Patient, Doctor, string);
+    MedicalRecord(int, Patient*, Doctor*, string);
     MedicalRecord(const MedicalRecord &obj);
     void setRecordID(int);
     void setDiagnosis(string);
