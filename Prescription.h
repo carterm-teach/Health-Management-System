a0a60d57b1e68ef86b60f4320c104abd8caf101c
Bobo-creator <demmyogunbade@gmail.com>
2026-04-20 22:06:19 -0500

    Fixed Methods defined inline AND in .cpp bug

--- Prescription.h
+++ Prescription.h
@@ -20,63 +20,12 @@ private:
 
 public:
 
-    // Default Constructor
-    Prescription()
-    {
-        prescriptionID = 0;
-        patient = nullptr;
-        doctor = nullptr;
-        medication = "Not Assigned";
-        dosage = "Not Assigned";
-        cout << "Default Prescription created." << endl;
-    }
-
-    // Parameterized Constructor
-    Prescription(int id, Patient* p, Doctor* d, string med, string dose)
-    {
-        prescriptionID = id;
-        patient = p;
-        doctor = d;
-        medication = med;
-        dosage = dose;
-        cout << "Prescription created successfully." << endl;
-    }
-
-    // Copy Constructor
-    Prescription(const Prescription& other)
-    {
-        prescriptionID = other.prescriptionID;
-        patient = other.patient;
-        doctor = other.doctor;
-        medication = other.medication;
-        dosage = other.dosage;
-        cout << "Prescription copied." << endl;
-    }
-
-    // Generate Summary
-    string generateSummary()
-    {
-        return "Prescription ID: " + to_string(prescriptionID) +
-               "\nMedication: " + medication +
-               "\nDosage: " + dosage;
-    }
-
-    // Display Details
-    void displayDetails()
-    {
-        cout << "----- Prescription Details -----" << endl;
-        cout << "ID: " << prescriptionID << endl;
-        cout << "Medication: " << medication << endl;
-        cout << "Dosage: " << dosage << endl;
-        cout << "--------------------------------" << endl;
-    }
-
-    // Destructor
-    ~Prescription()
-    {
-        cout << "Prescription ID " << prescriptionID
-             << " is being destroyed." << endl;
-    }
+    Prescription();
+    Prescription(int id, Patient* p, Doctor* d, string med, string dose);
+    Prescription(const Prescription& other);
+    ~Prescription();
+    string generateSummary();
+    void displayDetails();
 };
 
 #endif
