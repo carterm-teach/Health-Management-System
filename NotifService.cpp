776043cfbe7d980ba2957d4331c598e82e6d18be
Bobo-creator <demmyogunbade@gmail.com>
2026-05-01 14:22:40 -0500

    Update NotifService.cpp

--- NotifService.cpp
+++ NotifService.cpp
@@ -1,28 +1,25 @@
 #include "NotifService.h"
-#include "Appoinment.h"
-#include "Billing.h"
-#include "Patient.h"
 #include <iomanip>
 int NotifService::notificationsSent = 0;
 NotifService::NotifService()
 {
 cout << "NotifService started." << endl;
 }
-void NotifService::sendAppointmentReminder(const Appointment& appt)
+void NotifService::sendAppointmentReminder(const string& patientName, const string& doctorName, const string& appointmentDateTime)
 {
 cout << "\n========== APPOINTMENT REMINDER ==========" << endl;
-cout << "Appoinment ID: " << appt.getAppoinmentID() << endl;
-cout << "Date/Time: " << appt.getDateTime() << endl;
-cout << "Status: " << appt.getStatus() << endl;
+cout << "Patient: " << patientName << endl;
+cout << "Doctor: " << doctorName << endl;
+cout << "Date/Time: " << appointmentDateTime << endl;
 cout << "Reminder notification sent." << endl;
 notificationsSent++;
 }
-void NotifService::sendBillingNotification(const Billing& bill)
+void NotifService::sendBillingNotification(const string& patientName, double amountDue, const string& billStatus)
 {
 cout << "\n========== BILLING NOTIFICATION ==========" << endl;
-cout << "Bill ID: " << bill.getBillID() << endl;
-cout << "Amount Due: $" << bill.getAmount() << endl;
-cout << "Status: " << bill.getStatus << endl;
+cout << "Patient: " << patientName << endl;
+cout << "Amount Due: $" << fixed << setprecision(2) << amountDue << endl;
+cout << "Status: " << billStatus << endl;
 cout << "Billing notification sent." << endl;
 notificationsSent++;
 }
